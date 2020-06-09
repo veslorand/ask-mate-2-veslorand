512823a855b707da0373a4527b19abb6ef2b8723
veslorand <veslorand@gmail.com>
2020-06-09 14:39:28 +0200

    Add database_common.py

--- data_handler.py
+++ data_handler.py
@@ -1,8 +1,23 @@
 import datetime
 import os
 import uuid
+from typing import List, Dict
 
+from psycopg2 import sql
+from psycopg2.extras import RealDictCursor
 import connection
+import database_common
+
+
+@database_common.connection_handler
+def get_mentors(cursor: RealDictCursor) -> list:
+    query = """
+        SELECT first_name, last_name, city
+        FROM mentor
+        ORDER BY first_name"""
+    cursor.execute(query)
+    return cursor.fetchall()
+
 
 DATA_FOLDER_PATH = os.getenv('DATA_FOLDER_PATH') if 'DATA_FOLDER_PATH' in os.environ else './'
 QUESTION_FILE = DATA_FOLDER_PATH + "question.csv"
@@ -11,6 +26,8 @@ QUESTIONS_HEADER = ['id', 'submission_time', 'view_number', 'vote_number', 'titl
 ANSWERS_HEADER = ['id', 'submission_time', 'vote_number', 'question_id', 'message', 'image']
 ALLOWED_EXTENSIONS = {'png', 'jpg'}
 
+
+@database_common.connection_handler
 def get_questions_by_id(id, file_name):
     question = connection.read_csv_file(file_name)
     for dictionary in question:
@@ -18,6 +35,8 @@ def get_questions_by_id(id, file_name):
             return dictionary
 
 
+
+@database_common.connection_handler
 def get_answer_by_id(id, file_name):
     answer = connection.read_csv_file(file_name)
     for dict in answer:
@@ -25,6 +44,7 @@ def get_answer_by_id(id, file_name):
             return dict
 
 
+@database_common.connection_handler
 def get_all_question():
     try:
         return connection.read_csv_file(QUESTION_FILE)
@@ -33,6 +53,7 @@ def get_all_question():
         return []
 
 
+@database_common.connection_handler
 def get_all_answer():
     try:
         return connection.read_csv_file(ANSWER_FILE)
@@ -41,6 +62,7 @@ def get_all_answer():
         return []
 
 
+@database_common.connection_handler
 def find_answer_by_id(id, file_name):
     answer = connection.read_csv_file(file_name)
     for dic in answer:
@@ -48,15 +70,18 @@ def find_answer_by_id(id, file_name):
             return dic
 
 
+@database_common.connection_handler
 def get_random_id():
     return str(uuid.uuid4())
 
 
+@database_common.connection_handler
 def get_date_time():
     time = datetime.datetime.now()
     return str(time)
 
 
+@database_common.connection_handler
 def create_question_form(generator, filename):  # 'id', 'submission_time', 'view_number', 'vote_number', 'title', 'message', 'image'
     my_list = [get_random_id(), get_date_time(), '0', '0', filename]
     print(my_list)
@@ -66,6 +91,7 @@ def create_question_form(generator, filename):  # 'id', 'submission_time', 'view
     return my_list
 
 
+@database_common.connection_handler
 def create_answer_form(generator, question_id):  # 'id', 'submission_time', 'vote_number', 'question_id', 'message', 'image'
     my_list = [get_random_id(), get_date_time(), '0', question_id, '']
     title_and_message = [i for i in generator]
@@ -74,6 +100,7 @@ def create_answer_form(generator, question_id):  # 'id', 'submission_time', 'vot
     return my_list
 
 
+@database_common.connection_handler
 def edit_question(generator, question_id):
     question_by_id = get_questions_by_id(question_id, QUESTION_FILE)
     for dictionary in generator:
@@ -84,6 +111,7 @@ def edit_question(generator, question_id):
     return question_by_id
 
 
+@database_common.connection_handler
 def vote_up_question(question_id, file_name):
     question_dict = get_questions_by_id(question_id, file_name)
     for item in question_dict.items():
@@ -95,6 +123,7 @@ def vote_up_question(question_id, file_name):
     return question_dict
 
 
+@database_common.connection_handler
 def vote_down_question(question_id, file_name):
     question_dict = get_questions_by_id(question_id, file_name)
     for item in question_dict.items():
@@ -109,6 +138,7 @@ def vote_down_question(question_id, file_name):
     return question_dict
 
 
+@database_common.connection_handler
 def vote_up_answer(answer_id, file_name):
     answer_dict = get_answer_by_id(answer_id, file_name)
     for item in answer_dict.items():
@@ -120,6 +150,7 @@ def vote_up_answer(answer_id, file_name):
     return answer_dict
 
 
+@database_common.connection_handler
 def vote_down_answer(answer_id, file_name):
     answer_dict = get_answer_by_id(answer_id, file_name)
     for item in answer_dict.items():
@@ -134,6 +165,7 @@ def vote_down_answer(answer_id, file_name):
     return answer_dict
 
 
+@database_common.connection_handler
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
